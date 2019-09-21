6363565d68655bb9a5786d7fcb8c681f61203756
Shilina <twelidus@gmail.com>
2019-09-21 18:29:00 +0300

    Задание 8

---
+++ src/Task.java
@@ -0,0 +1,9 @@
+public class Task {
+    public static void main(String[] args) {
+        int i;
+        {
+            for (i = 1; i < 51; i++)
+                System.out.println(i);
+        }
+    }
+}
